c01c6441b74eea7e6077fd972d770e22682cbb22
Rajib <rajib@ubuntu>
2023-11-25 21:57:57 +0600

    cors and express.json middleware added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
-const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
 const port = process.env.PORT || 5000
 
 
-
+// middleware 
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fzaqft4.mongodb.net/?retryWrites=true&w=majority`;
 
