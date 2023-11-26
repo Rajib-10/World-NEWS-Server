9c934f9a65f7e4480b0c9da12e036f1dedacfbce
Rajib <rajib@ubuntu>
2023-11-26 17:13:59 +0600

    Make admin patch ednpoint added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
-
-// middleware 
-app.use(cors())
-app.use(express.json())
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fzaqft4.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -17,42 +16,55 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     await client.connect();
-    
+
     const userCollection = client.db("newsDB").collection("users");
 
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/users',async(req,res)=>{
-      const result = await userCollection.find().toArray()
-      res.send(result)
-    })
-
-app.post('/users',async(req,res)=>{
-    const user = req.body
-    const query = {email: user.email}
-      const existingUser = await userCollection.findOne(query)
-      if(existingUser){
-        return res.send({message: "User already exists",insertedId:null})
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists", insertedId: null });
       }
-      const result = await userCollection.insertOne(user)
-      res.send(result)
-})
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
-app.delete('/users/:id',async(req,res)=>{
-  const id = req.params.id
-  const query = {_id: new ObjectId(id)}
-  const result = await userCollection.deleteOne(query)
-  res.send(result)
-})
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // make admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -60,11 +72,10 @@ app.delete('/users/:id',async(req,res)=>{
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("World NEWS is running...");
+});
 
-app.get('/',(req,res)=>{
-    res.send("World NEWS is running...")
-})
-
-app.listen(port,()=>{
-    console.log(`World NEWS is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`World NEWS is running on port ${port}`);
+});
