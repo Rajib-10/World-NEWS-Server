1f1d8ffcc53c001319be9b7bfa77d8a97d35bd00
Rajib <rajib@ubuntu>
2023-11-26 15:53:11 +0600

    users delete endpoint added to delete user

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -42,6 +42,13 @@ app.post('/users',async(req,res)=>{
       }
       const result = await userCollection.insertOne(user)
       res.send(result)
+})
+
+app.delete('/users/:id',async(req,res)=>{
+  const id = req.params.id
+  const query = {_id: new ObjectId(id)}
+  const result = await userCollection.deleteOne(query)
+  res.send(result)
 })
 
     await client.db("admin").command({ ping: 1 });
