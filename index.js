a247fb9efbd229cde611ed0abc3029a2058c7cd5
Rajib <rajib@ubuntu>
2023-11-29 23:10:48 +0600

    jwt post endpoint added and response token

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const app = express();
+const jwt = require('jsonwebtoken')
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const app = express();
+
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -19,13 +21,70 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
+async function run()  { 
   try {
     await client.connect();
 
     const userCollection = client.db("newsDB").collection("users");
     const publishersCollection = client.db("newsDB").collection("publishers");
     const articleCollection = client.db("newsDB").collection("articles");
+
+
+
+       // jwt related api 
+
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body
+      const token = jwt.sign(user,process.env.SECRET_ACCESS_TOKEN,{expiresIn: '1hr'})
+      res.send({token})
+    })
+
+  // jwt middleware 
+
+  const verifyToken = (req,res,next)=>{
+     
+    
+    if(!req.headers.authorization){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = req.headers.authorization.split(' ')[1]
+
+    jwt.verify(token,process.env.SECRET_ACCESS_TOKEN,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({message: 'unauthorized access'})
+      }
+      req.decoded = decoded
+      next()
+    })
+  }
+
+
+  // verify admin .
+
+  const verifyAdmin = async(req,res,next)=>{
+    const email = req.decoded.email
+    const query = {email: email}
+    const user = await userCollection.findOne(query)
+    const isAdmin = user?.role==='admin'
+    if(!isAdmin){
+      return res.status(403).send({message: 'forbidden access'})
+    }
+    next()
+  }
+
+  app.get('/users/admin/:email',verifyToken,async(req,res)=>{
+    const email = req.params.email
+    if(email !== req.decoded.email){
+      return res.status(403).send({message: 'forbidden access'})
+    }
+    const query = {email: email}
+    const user = await userCollection.findOne(query)
+    let admin = false
+    if(user){
+      admin = user?.role === 'admin'
+    }
+    res.send({admin})
+  })
 
     app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
@@ -79,10 +138,50 @@ async function run() {
 
     // articles related api
 
-    app.get("/articles", async (req, res) => {
-      const result = await articleCollection.find().toArray();
-      res.send(result);
-    });
+  //   app.get("/articles", async (req, res) => {
+  //    try {
+  //   const searchQuery = req.query.search || ''; 
+  //   const regex = new RegExp(searchQuery, 'i'); 
+
+  //   const result = await articleCollection
+  //     .find({ title: { $regex: regex } })
+  //     .toArray();
+
+  //   res.send(result);
+  // } catch (error) {
+  //   console.error('Error fetching articles:', error);
+  //   res.status(500).send('Internal Server Error');
+  // }
+  //   });
+
+  app.get("/articles", async (req, res) => {
+  const { search, tags } = req.query;
+  const query = {};
+
+  if (search) {
+    // Add search query
+    query.title = { $regex: new RegExp(search, "i") };
+  }
+
+  if (tags) {
+    // Add tags query
+    query.tags = { $in: tags.split(",") };
+  }
+
+  const result = await articleCollection.find(query).toArray();
+  res.send(result);
+});
+
+    // getting top views 6 articles
+
+    app.get('/articles/top-views', async (req, res) => {
+  try {
+    const topViewsArticles = await articleCollection.find().sort({ views: -1 }).limit(6).toArray();
+    res.send(topViewsArticles);
+  } catch (error) {
+    console.error('Error fetching top views articles:', error);
+  }
+});
 
     app.post('/articles',async(req,res)=>{
       const article = req.body; 
@@ -97,6 +196,19 @@ async function run() {
       res.send(result);
     });
 
+    // article view 
+    app.patch('/articles/views/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $inc: { views: 1 }
+      };
+      const result = await articleCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+
+    })
+
+    // article update 
     app.patch("/articles/:id",async(req,res)=>{
       const id = req.params.id;
       const data = req.body
